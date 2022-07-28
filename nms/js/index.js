51f88c269dc4153fda3056e05f43903f0adad1fa
Dydier Ripe <82624734+DydierRipe@users.noreply.github.com>
2022-07-28 17:12:28 -0500

    Update index.js

--- nms/js/index.js
+++ nms/js/index.js
@@ -1,5 +1,8 @@
 "use strict"
 
+let ball = document.querySelector(".ball");
+let chart = document.querySelector(".proofs-chart");
+
 let properties = {
     "ambience" : {
         "gravity" : 9.8,
@@ -7,10 +10,12 @@ let properties = {
     "ball" : {
         "dimentions" : [5, 5],
         "weight" : 30
+    },
+    "chart" : {
+        "sizePx" : [chart.style.width, chart.style.height],
+        "sizeReal" : [145, 111.67]
     }
 }
-
-let ball = document.querySelector(".ball");
 
 let transformer = (posX, posY) => { // 1m = 6px
     let posYpx = 670 - posY * 6, posXpx = posX * 6;
@@ -18,6 +23,8 @@ let transformer = (posX, posY) => { // 1m = 6px
     ball.style.transform = `translate(${posXpx}px, ${posYpx}px)`;
 }
 
-let freeFall = () => {
-
+let startSimulation = (condition, callBack, ...callBackVars) => {
+    while (condition) {
+        setTimeOut(callBack(callBackVars), 10);
+    }
 }
